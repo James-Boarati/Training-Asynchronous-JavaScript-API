42a41f8bc660af8d2b3d672832314c2037d2598c
James-Boarati <runaway_james@hotmail.com>
2023-09-02 20:45:35 -0300

    Tratando erros com catch

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var buscadorCep = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(r => r.json())
-.then(r => console.log(r));
+var buscadorCep = fetch('https://viacep.com.br/ws/03615030/json/')
+    .then(r => r.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error ('Esse cep não existe!')
+        } else
+        console.log(r)})
+    .catch(erro => console.log(erro)); //metodo catch = pegue. Pegue o erro e mostre na tela.
 
 console.log(buscadorCep)
