5f01744b1e0739b39548b4f7cb04e12a60d82265
James-Boarati <runaway_james@hotmail.com>
2023-09-04 15:16:52 -0300

    Preenchimento automatico, projeto final do curso

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 async function buscadorCEP (cep) {
+    var mensagemErro = document.getElementById('erro');
+    
     try {
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`)
     var convertendoCEP = await consultaCEP.json();
     if (convertendoCEP.erro) {
-        throw Error('CEP não existente!')
+        throw Error('CEP não existente!');
     }
+    var localidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+    var bairro = document.getElementById('bairro')
+
+    localidade.value = convertendoCEP.localidade;
+    logradouro.value = convertendoCEP.logradouro;
+    estado.value = convertendoCEP.uf;
+    bairro.value = convertendoCEP.bairro;
+
     console.log(convertendoCEP);
     return convertendoCEP;
 } catch (erro){
+    mensagemErro.innerHTML = `<p>CEP inválido ou inexistente. Tente novamente! </p>`
     console.log(erro)
 }
 }
 
 var cep = document.getElementById('cep');
-cep.addEventListener ("focusout", () => buscadorCEP(cep.value));
-
+cep.addEventListener ("focusout", () => buscadorCEP(cep.value)); //detectando interação do usúario com auxílio do addEventListener e
+//Incluindo valores em elementos com a propriedade value;
